cd8ca106b9d5380904fdcbd6871f68f329e48f10
Dimirolz <dimirolz@gmail.com>
2024-07-01 17:15:38 +0400

    refactor: win logic

--- src/features/board/boardSlice.ts
+++ src/features/board/boardSlice.ts
@@ -23,6 +23,77 @@ const initialState: BoardState = {
 	isDraw: false,
 }
 
+type TLoopBoardProps = {
+	board: CellValue[][]
+	cellValue: CellValue
+	row: number
+	col: number
+	rowDir: number
+	colDir: number
+	cb: (rowIndex: number, colIndex: number) => void
+}
+
+function traverseConsecutiveCells({
+	board,
+	cellValue,
+	row,
+	col,
+	rowDir,
+	colDir,
+	cb,
+}: TLoopBoardProps) {
+	let changedDir = false
+	let leftPointer = 1
+	let rightPointer = 0
+	let i = 0
+
+	while (i < 4) {
+		// @NOTE: wow. ts cannot infer this variable
+		const currentPointer: number = changedDir ? leftPointer : rightPointer
+		const rowIndex = row + currentPointer * rowDir
+		const colIndex = col + currentPointer * colDir
+
+		if (board[rowIndex]?.[colIndex] === cellValue) {
+			cb(rowIndex, colIndex)
+			i++
+			changedDir ? leftPointer++ : rightPointer++
+		} else if (!changedDir) {
+			// change direction only once
+			;[rowDir, colDir] = [-rowDir, -colDir]
+			changedDir = true
+		} else {
+			break // stop if there is a non-matching cell after changing direction
+		}
+	}
+}
+
+type TCheckDirectionProps = Omit<TLoopBoardProps, 'cb'>
+
+function checkDirection(props: TCheckDirectionProps) {
+	let count = 0
+	traverseConsecutiveCells({...props, cb: () => count++})
+	return count === 4
+}
+
+type TMarkWinCellsProps = Omit<TLoopBoardProps, 'cb'> & {
+	winCellValue: CellValue
+}
+
+function markWinCells({board, winCellValue, ...props}: TMarkWinCellsProps) {
+	traverseConsecutiveCells({
+		board,
+		...props,
+		cb: (rowIndex, colIndex) => {
+			board[rowIndex][colIndex] = winCellValue
+		},
+	})
+}
+
+function isDraw(board: CellState[][]) {
+	// @NOTE: we can optimize isDraw check, but I'm fine with current impl
+	return board.every((row) => row.every((cell) => cell !== CellState.EMPTY))
+}
+
 export const boardSlice = createSlice({
 	name: 'board',
 	initialState,
@@ -34,189 +105,48 @@ export const boardSlice = createSlice({
 			const winCellValue =
 				playerCellValue === CellState.BLACK ? CellState.BLACK_WIN : CellState.RED_WIN
 
-			for (let row = BOARD_HEIGHT - 1; row >= 0; row--) {
+			outer: for (let row = BOARD_HEIGHT - 1; row >= 0; row--) {
 				if (state.board[row][col] === CellState.EMPTY) {
 					state.board[row][col] = playerCellValue
 
-					// check for the win
-					// oh way. I should refactor this to generic function
-					let count = 0
-					// vertical first
-					for (let i = row; i < BOARD_HEIGHT; i++) {
-						if (count === 4) break
-						if (state.board[i][col] === playerCellValue) count++
-						else break
-					}
+					const directions = [
+						{rowDir: 0, colDir: 1}, // horizontal
+						{rowDir: 1, colDir: 0}, // vertical
+						{rowDir: 1, colDir: 1}, // right diagonal
+						{rowDir: 1, colDir: -1}, // left diagonal
+					]
 
-					if (count === 4) {
-						for (let i = row; i < BOARD_HEIGHT; i++) {
-							if (count === 0) break
-							if (state.board[i][col] === playerCellValue) {
-								state.board[i][col] = winCellValue
-								count--
-							} else break
+					for (const {rowDir, colDir} of directions) {
+						const props = {
+							board: state.board,
+							cellValue: playerCellValue,
+							row,
+							col,
+							rowDir,
+							colDir,
 						}
-
-						state.isWin = true
-						break
-					}
-
-					// we are not checking other directions if we already win;
-
-					// horizontal
-					count = 0
-					for (let i = col; i < BOARD_WIDTH; i++) {
-						if (count === 4) break
-						if (state.board[row][i] === playerCellValue) count++
-						else break
-					}
-
-					for (let i = col - 1; i >= 0; i--) {
-						if (count === 4) break
-						if (state.board[row][i] === playerCellValue) count++
-						else break
-					}
-
-					if (count === 4) {
-						for (let i = col - 1; i >= 0; i--) {
-							if (count === 0) break
-							if (state.board[row][i] === playerCellValue) {
-								state.board[row][i] = winCellValue
-								count--
-							} else break
+						if (checkDirection(props)) {
+							markWinCells({
+								...props,
+								winCellValue,
+							})
+							state.isWin = true
+							break outer
 						}
-						for (let i = col; i < BOARD_WIDTH; i++) {
-							if (count === 0) break
-							if (state.board[row][i] === playerCellValue) {
-								state.board[row][i] = winCellValue
-								count--
-							} else break
-						}
-						state.isWin = true
-						break
 					}
 
-					// diagonal left to rigth
-					count = 0
-					let colDir = 1
-					let rowDir = -1
-					for (let i = 0; i < 4; i++) {
-						const rowIndex = row + i * rowDir
-						const colIndex = col + i * colDir
-						if (count === 4) break
-						if (state.board[rowIndex]?.[colIndex] === playerCellValue) count++
-						else break
+					if (isDraw(state.board)) {
+						state.isDraw = true
+					} else {
+						state.player = state.player === 'black' ? 'red' : 'black'
 					}
 
-					colDir = -1
-					rowDir = 1
-					// we should skip center;
-					// we can do this for both direction and start from next cell. current cell is always good.
-					// wow those two loops are the same. now almost. that's cool
-					for (let i = 0; i < 4; i++) {
-						const rowIndex = row + 1 + i * rowDir
-						const colIndex = col - 1 + i * colDir
-						if (count === 4) break
-						if (state.board[rowIndex]?.[colIndex] === playerCellValue) count++
-						else break
-					}
-
-					if (count === 4) {
-						colDir = 1
-						rowDir = -1
-						for (let i = 0; i < 4; i++) {
-							const rowIndex = row + i * rowDir
-							const colIndex = col + i * colDir
-							if (count === 0) break
-							if (state.board[rowIndex]?.[colIndex] === playerCellValue) {
-								state.board[rowIndex][colIndex] = winCellValue
-								count--
-							} else break
-						}
-						colDir = -1
-						rowDir = 1
-						for (let i = 0; i < 4; i++) {
-							const rowIndex = row + 1 + i * rowDir
-							const colIndex = col - 1 + i * colDir
-							if (count === 0) break
-							if (state.board[rowIndex]?.[colIndex] === playerCellValue) {
-								state.board[rowIndex][colIndex] = winCellValue
-								count--
-							} else break
-						}
-						state.isWin = true
-						break
-					}
-
-					// diagonal right to left
-					count = 0
-					colDir = -1
-					rowDir = -1
-					for (let i = 0; i < 4; i++) {
-						const rowIndex = row + i * rowDir
-						const colIndex = col + i * colDir
-						console.log(rowIndex, colIndex, 'diagonal forward')
-						if (count === 4) break
-						if (state.board[rowIndex]?.[colIndex] === playerCellValue) count++
-						else break
-					}
-
-					colDir = 1
-					rowDir = 1
-					// we should skip center;
-					// we can do this for both direction and start from next cell. current cell is always good.
-					// wow those two loops are the same. now almost. that's cool
-					for (let i = 0; i < 4; i++) {
-						const rowIndex = row + 1 + i * rowDir
-						const colIndex = col + 1 + i * colDir
-						console.log(rowIndex, colIndex, 'diagonal backward')
-						if (count === 4) break
-						if (state.board[rowIndex]?.[colIndex] === playerCellValue) count++
-						else break
-					}
-
-					if (count === 4) {
-						colDir = -1
-						rowDir = -1
-						for (let i = 0; i < 4; i++) {
-							const rowIndex = row + i * rowDir
-							const colIndex = col + i * colDir
-							if (count === 0) break
-							if (state.board[rowIndex]?.[colIndex] === playerCellValue) {
-								state.board[rowIndex][colIndex] = winCellValue
-								count--
-							} else break
-						}
-						colDir = 1
-						rowDir = 1
-						for (let i = 0; i < 4; i++) {
-							const rowIndex = row + 1 + i * rowDir
-							const colIndex = col + 1 + i * colDir
-							console.log(rowIndex, colIndex, 'diagonal')
-							if (count === 0) break
-							if (state.board[rowIndex]?.[colIndex] === playerCellValue) {
-								state.board[rowIndex][colIndex] = winCellValue
-								count--
-							} else break
-						}
-						state.isWin = true
-						break
-					}
-
-					// check for draw
-
-					if (state.board.every((row) => row.every((cell) => cell !== CellState.EMPTY))) {
-						state.isDraw = true;
-						break;
-					}
-
-					state.player = state.player === 'black' ? 'red' : 'black'
 					break
 				}
 			}
 		},
 		restartGame: () => {
-			return initialState;
+			return initialState
 		},
 	},
 })
