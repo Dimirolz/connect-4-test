5618032bf6d3f6812abd78d7bd1c5ac4615d242a
Dimirolz <dimirolz@gmail.com>
2024-07-02 13:04:01 +0400

    feat: header styling

--- src/components/BoardHeader.tsx
+++ src/components/BoardHeader.tsx
@@ -1,13 +1,42 @@
 import {useAppSelector} from '../hooks/store'
+import {cn} from '../utils'
 
 export default function BoardHeader() {
 	const currentPlayer = useAppSelector((state) => state.board.player)
 	const isWin = useAppSelector((state) => state.board.isWin)
 	const isDraw = useAppSelector((state) => state.board.isDraw)
 
-	return (
-		<h1 className="mb-4 text-2xl font-bold">
-			{isDraw ? 'Draw' : isWin ? `Winner: ${currentPlayer}` : `Current Player: ${currentPlayer}`}
-		</h1>
-	)
+	const renderStatusMessage = () => {
+		if (isDraw) {
+			return 'Draw'
+		}
+
+		if (isWin) {
+			return (
+				<>
+					Winner:{' '}
+					<span
+						className={cn('font-bold', currentPlayer === 'black' ? 'text-black' : 'text-red-500')}>
+						{currentPlayer}
+					</span>
+				</>
+			)
+		}
+
+		return (
+			<>
+				Player{' '}
+				<span
+					className={cn(
+						'inline-flex min-w-[92px] justify-center rounded-full bg-gray-200 px-4 align-bottom font-bold',
+						currentPlayer === 'black' ? 'text-black' : 'text-red-500',
+					)}>
+					{currentPlayer}
+				</span>{' '}
+				turn
+			</>
+		)
+	}
+
+	return <h1 className="mb-4 text-2xl font-bold">{renderStatusMessage()}</h1>
 }
