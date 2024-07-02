4eddf61b6367d7c43fe3fa746cfa35046bba5097
Dimirolz <dimirolz@gmail.com>
2024-07-02 13:04:12 +0400

    feat: game description

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
-// @TODO: create game history
-// @TODO: write tests for 4 type of wins and for draw
-
 import Board from './components/Board'
 
 export default function App() {
 	return (
-		<div className="container flex h-screen items-center justify-center">
+		<div className="container flex flex-col h-screen items-center justify-center">
 			<Board />
+
+			<p className="text-center text-sm text-gray-500 mt-4 max-w-[350px]">
+				It’s the classic game you love to play with your friends and family. Can you connect four of
+				your coloured disks by dropping them into the holder before your opponent does?
+			</p>
 		</div>
 	)
 }
